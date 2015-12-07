2c048019a09be46d8535f02624a1a96bda96cdfe
Owoade Dotun <dotun.owoade@andela.com>
2015-12-07 16:13:12 +0100

    refactored helper.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,8 @@
+"use strict";
 var readFileSync = function(file) {
   var content;
   $.ajax({
-    dataType: "json",
+    dataType: "json",s
     async: false,
     url: file,
     success: function(result) {
@@ -9,16 +10,17 @@ var readFileSync = function(file) {
     }
   });
   return content;
-}
+};
 
 Array.prototype.unique = function() {
   var newarr = [];
   for (var i = 0, l = this.length; i < l; i++)
-    if (newarr.indexOf(this[i]) === -1 && this[i] !== '')
+    if (newarr.indexOf(this[i]) === -1 && this[i] !== "")
       newarr.push(this[i]);
   return newarr;
-}
+};
 
 Array.prototype.intersect = function(array) {
   return this.filter((x) => array.indexOf(x) != -1);
-}
+};
+
